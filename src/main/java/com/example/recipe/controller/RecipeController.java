911b50d62025a91ee89b73012c2ca54b4b4ad915
Masoud Afrashteh <massoud.afrashteh@gmail.com>
2022-08-04 11:11:34 +0430

    added recipe find all and find one

--- src/main/java/com/example/recipe/controller/RecipeController.java
+++ src/main/java/com/example/recipe/controller/RecipeController.java
@@ -4,11 +4,13 @@ import com.example.recipe.controller.request.RecipeDto;
 import com.example.recipe.model.Recipe;
 import com.example.recipe.service.RecipeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,7 +29,7 @@ public class RecipeController {
             tags = {"recipes"},
             responses = {
                     @ApiResponse(responseCode = "201", description = "New recipe created", content = {
-                            @Content(mediaType = "application/json", schema = @Schema(implementation = RecipeDto.class))
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = Recipe.class))
                     }),
                     @ApiResponse(responseCode = "400", description = "Invalid input/object is sent", content = {
                             @Content(mediaType = "application/json", schema = @Schema(implementation = Object.class))
@@ -38,4 +40,34 @@ public class RecipeController {
         return new ResponseEntity<>(recipeService.createRecipe(recipeDto), HttpStatus.CREATED);
     }
 
+    @GetMapping
+    @Operation(
+            operationId = "getAllRecipes",
+            summary = "get All Recipes",
+            tags = {"recipes"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Returns recipes list")
+            }
+    )
+    public ResponseEntity<Page<Recipe>> getAllRecipes(
+            @Parameter(name = "pageSize", example = "10") @RequestParam(required = false) Integer pageSize,
+            @Parameter(name = "pageNumber", example = "0") @RequestParam(required = false) Integer pageNumber) {
+        return new ResponseEntity<>(recipeService.getAllRecipes(pageSize, pageNumber), HttpStatus.OK);
+    }
+
+    @GetMapping("/{recipeId}")
+    @Operation(
+            operationId = "getRecipe",
+            summary = "get existed Recipe",
+            tags = {"recipes"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Return only one recipe", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = Recipe.class))
+                    })
+            }
+    )
+    public ResponseEntity<Recipe> getRecipe(
+            @Parameter(name = "recipeId", example = "1", description = "recipe id", required = true) @PathVariable("recipeId") Long recipeId) {
+        return new ResponseEntity<>(recipeService.getRecipeById(recipeId), HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/recipe/service/RecipeService.java
+++ src/main/java/com/example/recipe/service/RecipeService.java
@@ -1,12 +1,15 @@
 package com.example.recipe.service;
 
 import com.example.recipe.controller.request.RecipeDto;
+import com.example.recipe.exception.EntityNotFoundException;
 import com.example.recipe.model.Ingredient;
 import com.example.recipe.model.Recipe;
 import com.example.recipe.repository.IngredientRepository;
 import com.example.recipe.repository.RecipeRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,16 +24,25 @@ public class RecipeService {
     @Autowired
     private ObjectMapper objectMapper;
 
+    private Recipe getRecipeByDto(RecipeDto recipeDto) {
+        return objectMapper.convertValue(recipeDto, Recipe.class);
+    }
+
     @Transactional
     public Recipe createRecipe(RecipeDto recipeDto) {
-        Recipe recipeEntity = getRecipe(recipeDto);
+        Recipe recipeEntity = getRecipeByDto(recipeDto);
         for (Ingredient ingredient : recipeEntity.getIngredients()) {
             ingredient.setRecipe(recipeEntity);
         }
         return recipeRepository.save(recipeEntity);
     }
 
-    private Recipe getRecipe(RecipeDto recipeDto) {
-        return objectMapper.convertValue(recipeDto, Recipe.class);
+    public Page<Recipe> getAllRecipes(int pageSize, int pageNumber) {
+        return recipeRepository
+                .findAll(PageRequest.of(pageNumber, pageSize == 0 ? 10 : pageSize));
+    }
+
+    public Recipe getRecipeById(Long recipeId) {
+        return recipeRepository.findById(recipeId).orElseThrow(() -> new EntityNotFoundException("Could not find recipe with id: " + recipeId));
     }
 }
